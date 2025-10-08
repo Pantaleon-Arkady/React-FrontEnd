279d2c989e19a4a5738b95cf3520b775134f60fe
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-08 21:13:12 +0800

    default tasks, import TaskList

--- src/TaskApp.jsx
+++ src/TaskApp.jsx
@@ -1,9 +1,20 @@
+import TaskList from "./TaskList";
+
 function TaskApp() {
+    const defaultTasks = [
+        { id: 1, text: "Talent is a pursued interest." },
+        { id: 2, text: "Anything you are willing to practice you can do." },
+        { id: 3, text: "I can't do this, I can't do it — that's balogna." },
+        { id: 4, text: "- Bob Ross" }
+    ];
+
+
     return (
         <div>
-            <span>
-                Task List Components
-            </span>
+            <h2>Task List</h2>
+            <TaskList 
+                tasks={defaultTasks}
+            />
         </div>
     )
 }
