dc6c7bc80f671958bcdf8f962e4a5ee326de6525
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-14 17:26:59 +0800

    reminders app init...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import TaskApp from "./TaskApp";
+import ReminderApp from "./ReminderApp";
 
 function Home() {
     return (
         <div>
             <h2>Initial Page</h2>
             <Link to="/task-app" className="btn btn-primary m-2" >Task App</Link>
+            <Link to="/reminder-app" className="btn btn-primary m-2" >Reminder App</Link>
         </div>
     )
 }
@@ -17,6 +19,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/task-app" element={<TaskApp/>} />
+                <Route path="/reminder-app" element={<ReminderApp/>} />
             </Routes>
         </Router>
     )
---
+++ src/ReminderApp.jsx
@@ -0,0 +1,10 @@
+function ReminderApp() {
+
+    return (
+        <div>
+            <span>Reminders App Init</span>
+        </div>
+    )
+}
+
+export default ReminderApp;
