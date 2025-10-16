42b2d7f6ff3015bc6627989d10c8bfd9012c3e77
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-16 22:36:15 +0800

    styled reminder data display orientation...

--- src/ReminderList.jsx
+++ src/ReminderList.jsx
@@ -5,10 +5,18 @@ function ReminderList({ reminders }) {
     return (
         <ul className="list-group mt-3">
             {reminders.map(reminder => (
-                <li className="list-group-item ">
-                    <span>
-                        {reminder.text}
-                    </span>
+                <li className="list-group-item d-flex">
+                    <div className="reminders_text border">
+                        <span>
+                            {reminder.text}
+                        </span>
+                    </div>
+                    <div className="reminders_date border">
+
+                    </div>
+                    <div className="reminders_mod border">
+
+                    </div>
                 </li>
             ))}
         </ul>
