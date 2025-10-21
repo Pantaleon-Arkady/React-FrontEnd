c0209e6679ea8832c136aae54085751a1b7524b9
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-21 11:35:24 +0800

    Edit init...

---
+++ src/ReminderEdit.jsx
@@ -0,0 +1,12 @@
+function ReminderEdit() {
+
+    return (
+        <form>
+            <span>
+                Edit Form...
+            </span>
+        </form>
+    )
+}
+
+export default ReminderEdit;
