15ebbe7d134f72dc299ea55218c4cf4b0d3bb1e1
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-26 15:25:13 +0800

    for let loop logic to duplicate left features div

--- src/components/WorkoutApp.jsx
+++ src/components/WorkoutApp.jsx
@@ -1,14 +1,33 @@
-
-
 function WorkoutApp() {
+
+    const leftFeatures = [];
+
+    for (let ctr = 0; ctr < 9; ctr++) {
+        leftFeatures.push(
+            <div key={ctr} className="border p-4 mx-3 mt-4">
+                Left Feature {ctr + 1}
+            </div>
+        )
+    }
 
     return (
         <div className="workout_main_div d-flex flex-column">
-            <div className="w-100 p-3 border">Header Part</div>
+            <div className="w-100 p-3 border d-flex flex-row justify-content-between">
+                <div className="w-25 border">
+                    Workout App
+                </div>
+                <div className="w-50 border d-flex flex-row justify-content-end">
+                    <button className="btn btn-primary me-2">Features</button>
+                    <button className="btn btn-primary me-2">Features</button>
+                    <button className="btn btn-primary me-2">Features</button>
+                </div>
+            </div>
             <div className="w-100 d-flex workout_contents">
-                <div className="w-25 border">Left</div>
-                <div className="w-50 border">Main</div>
-                <div className="w-25 border">Right</div>
+                <div className="w-25 border d-flex flex-column content_divs">
+                    {leftFeatures}
+                </div>
+                <div className="w-50 border content_divs">Main</div>
+                <div className="w-25 border content_divs">Right</div>
             </div>
         </div>
     )
