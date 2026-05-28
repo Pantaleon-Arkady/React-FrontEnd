05eb8e85bc4c77084cde33810d37a3fc21b937e2
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-28 13:36:00 +0800

    basic orientation

--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -12,10 +12,25 @@ function Operation() {
                 </div>
                 <div>Operations</div>
                 <div>
-                    
+
                 </div>
             </div>
-            
+            <div className="w-100 border d-flex flex-column p-5">
+                <div className="w-100 border d-flex flex-row">
+                    <div className="w-75 border">
+                        Numbers
+                    </div>
+                    <div className="w-25 border">
+                        Operations
+                    </div>
+                </div>
+                <div className="w-100 border d-flex flex-row p-2 mt-5">
+                    <div className="w-25 border">Number</div>
+                    <div className="w-25 border">Operation</div>
+                    <div className="w-25 border">Number</div>
+                    <div className="w-25 border">Result</div>
+                </div>
+            </div>
         </div>
     )
 }
