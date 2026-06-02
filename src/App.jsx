b66ba31008b243850f7576b9229f5ba0e89e16ec
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-06-02 11:49:12 +0800

    added First Layout component, also changed layout/oreination of initial page for component links

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,26 @@ import ReminderApp from "./components/ReminderApp";
 import WorkoutApp from "./components/WorkoutApp";
 import Operation from "./components/Operations";
 import Trials from "./components/Trials";
+import FirstLayout from "./components/FirstLayout";
 
 function Home() {
     return (
-        <div>
+        <div className="d-flex flex-column">
             <h2>Initial Page</h2>
-            <Link to="/task-app" className="btn btn-primary m-2" >Task App</Link>
-            <Link to="/reminder-app" className="btn btn-primary m-2" >Reminder App</Link>
-            <Link to="/workout-app" className="btn btn-primary m-2">Workout App</Link>
-            <Link to="/operation-app" className="btn btn-primary m-2">Operation App</Link>
-            <Link to="/trials" className="btn btn-primary m2">Trials </Link>
+            <div className="d-flex flex-row">
+                <div className="d-flex w-25 flex-column p-5 m-3 bg-light rounded border border-2">
+                    <h2>React JS Apps</h2>
+                    <Link to="/task-app" className="btn btn-primary m-2" >Task App</Link>
+                    <Link to="/reminder-app" className="btn btn-primary m-2" >Reminder App</Link>
+                    <Link to="/workout-app" className="btn btn-primary m-2">Workout App</Link>
+                    <Link to="/operation-app" className="btn btn-primary m-2">Operation App</Link>
+                    <Link to="/trials" className="btn btn-primary m-2">Trials </Link>
+                </div>
+                <div className="d-flex w-25 flex-column p-5 m-3 bg-light rounded border border-2">
+                    <h2>React JS Layouts</h2>
+                    <Link to="/first-layout" className="btn btn-primary m-2">First Layout</Link>
+                </div>
+            </div>
         </div>
     )
 }
@@ -29,6 +39,7 @@ function App() {
                 <Route path="/workout-app" element={<WorkoutApp />} />
                 <Route path="/operation-app" element={<Operation />} />
                 <Route path="/trials" element={<Trials />} />
+                <Route path="/first-layout" element={<FirstLayout />} />
             </Routes>
         </Router>
     )
