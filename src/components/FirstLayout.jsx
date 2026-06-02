5bb24038d85737a397f069f335b2fb9b4d294d96
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-06-02 11:54:28 +0800

    First layout component init

---
+++ src/components/FirstLayout.jsx
@@ -0,0 +1,10 @@
+function FirstLayout() {
+
+    return (
+        <div className="w-100 vh-100 border bg-light border border-black">
+            First Layout
+        </div>
+    )
+}
+
+export default FirstLayout;
