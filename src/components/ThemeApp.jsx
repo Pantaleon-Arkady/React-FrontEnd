d6cbb2bda7043205fa42755269a8ce2fdd0a574c
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-06-07 17:41:27 +0800

    Basic layout with dropdown for theme change

--- src/components/ThemeApp.jsx
+++ src/components/ThemeApp.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 function ThemeApp() {
+    const [themeValue, setThemeValue] = useState("Light");
 
     return (
-        <div>
-            Theme app
+        <div className="w-100 vh-100 bg-secondary-subtle">
+            <div className="border rounded m-2 bg-white p-3">
+                Theme App Header
+            </div>
+            <div className="border rounded m-2 bg-white p-4">
+                Theme App Body
+                <button type="button" id="themeButton" className="btn btn-secondary dropdown-toggle border m-2" data-bs-toggle="dropdown">
+                    Theme: <span>{ themeValue } </span>
+                </button>
+                <div className="dropdown-menu">
+                    <button onClick={() => setThemeValue("Light")} className="dropdown-item">Light</button>
+                    <button onClick={() => setThemeValue("Dark")} className="dropdown-item">Dark</button>
+                    <button onClick={() => setThemeValue("Blue")} className="dropdown-item">Blue</button>
+                </div>
+                App
+            </div>
         </div>
     )
 }
