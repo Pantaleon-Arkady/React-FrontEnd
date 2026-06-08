6cbe888459d27d72244ee107dbb92e0139f4f9c9
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-06-08 15:54:20 +0800

    theme app, mostly style theme changing by reducer, have 3 themes instead of usual light and dark

--- src/components/ThemeApp.jsx
+++ src/components/ThemeApp.jsx
@@ -1,22 +1,68 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 function ThemeApp() {
-    const [themeValue, setThemeValue] = useState("Light");
+
+    const themeReducer = (state, action) => {
+        switch (action.type) {
+            case "LIGHT":
+                return {
+                    mode: "Light",
+                    bg: "secondary-subtle"
+                };
+
+            case "DARK":
+                return {
+                    mode: "Dark",
+                    bg: "dark"
+                };
+
+            case "BLUE":
+                return {
+                    mode: "Blue",
+                    bg: "primary"
+                };
+
+            default:
+                return state;
+        }
+    };
+
+    const [theme, dispatch] = useReducer(themeReducer, {
+        mode: "Light",
+        bg: "secondary-subtle"
+    });
 
     return (
-        <div className="w-100 vh-100 bg-secondary-subtle">
+        <div className={`w-100 vh-100 bg-${theme.bg}`}>
             <div className="border rounded m-2 bg-white p-3">
                 Theme App Header
             </div>
             <div className="border rounded m-2 bg-white p-4">
                 Theme App Body
                 <button type="button" id="themeButton" className="btn btn-secondary dropdown-toggle border m-2" data-bs-toggle="dropdown">
-                    Theme: <span>{ themeValue } </span>
+                    Theme: {theme.mode}
                 </button>
                 <div className="dropdown-menu">
-                    <button onClick={() => setThemeValue("Light")} className="dropdown-item">Light</button>
-                    <button onClick={() => setThemeValue("Dark")} className="dropdown-item">Dark</button>
-                    <button onClick={() => setThemeValue("Blue")} className="dropdown-item">Blue</button>
+                    <button
+                        onClick={() => dispatch({ type: "LIGHT" })}
+                        className="dropdown-item"
+                    >
+                        Light
+                    </button>
+
+                    <button
+                        onClick={() => dispatch({ type: "DARK" })}
+                        className="dropdown-item"
+                    >
+                        Dark
+                    </button>
+
+                    <button
+                        onClick={() => dispatch({ type: "BLUE" })}
+                        className="dropdown-item"
+                    >
+                        Blue
+                    </button>
                 </div>
                 App
             </div>
