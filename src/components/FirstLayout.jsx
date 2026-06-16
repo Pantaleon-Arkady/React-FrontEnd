e46122133864eac347d8d8cceb72f698811d3118
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-06-16 22:13:54 +0800

    basic layout

--- src/components/FirstLayout.jsx
+++ src/components/FirstLayout.jsx
@@ -1,10 +1,133 @@
 function FirstLayout() {
-
     return (
-        <div className="w-100 vh-100 border bg-light border border-black">
-            <div className="bg-white first_header m-2 border">Header</div>
+        <div className="vh-100 d-flex flex-column bg-light">
+            <header
+                className="bg-white border m-2 flex-shrink-0"
+                style={{ height: "70px" }}
+            >
+                Header
+            </header>
+            <main className="flex-grow-1 d-flex gap-2 px-2 pb-2 overflow-hidden">
+                {/* Left 25% */}
+                <aside
+                    className="bg-white border overflow-auto"
+                    style={{ flex: "0 0 25%" }}
+                >
+                    Left Content
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                    <div className="m-2 border p-3">
+                        Feature
+                    </div>
+                </aside>
+                <section
+                    className="bg-white border overflow-auto"
+                    style={{ flex: "0 0 50%" }}
+                >
+                    Main Content
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                    <div className="mx-3 my-4 p-3 border h-25">
+                        Main Feature Div
+                    </div>
+                </section>
+                <aside
+                    className="bg-white border overflow-auto"
+                    style={{ flex: "0 0 25%" }}
+                >
+                    <div>
+                        Right Feature
+                    </div>
+                </aside>
+            </main>
         </div>
-    )
+    );
 }
 
 export default FirstLayout;
