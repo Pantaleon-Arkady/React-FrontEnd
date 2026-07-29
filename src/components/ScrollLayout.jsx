ba8a50c2ea6ab7c09e91ddb18bef3715767728eb
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-07-29 11:25:31 +0800

    1st div mobile and desktop display fluid

--- src/components/ScrollLayout.jsx
+++ src/components/ScrollLayout.jsx
@@ -7,15 +7,15 @@ function ScrollLayout() {
                     Header
                 </div>
 
-                <div className="border second_content_divs">
-                    <div className="second_left_content_div border">
-                        <div className="second_left_content border border-4">
+                <div className="second_content_divs">
+                    <div className="second_left_content_div">
+                        <div className="second_left_content">
                             Card Content
                         </div>
                     </div>
 
-                    <div className="second_right_content_div border">
-                        <div className="second_right_content border">
+                    <div className="second_right_content_div">
+                        <div className="second_right_content">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada rutrum ipsum sed pulvinar. Maecenas sit amet aliquam tortor. Cras leo odio, varius non purus in, mollis maximus libero. In sed commodo velit. Sed semper nisl ac placerat luctus. Sed nulla lectus, faucibus a sapien ut, ornare dignissim velit. Nunc tempus arcu quam, at consequat felis eleifend id. Aenean volutpat gravida libero, non dignissim nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut faucibus suscipit nulla. Vivamus dignissim nisl urna, quis sodales eros eleifend sit amet. Suspendisse ipsum est, convallis ut efficitur vel, tristique in metus. Sed cursus laoreet sem, ut rhoncus nulla finibus et. Fusce vulputate imperdiet dignissim. Aliquam iaculis magna sed iaculis vulputate.
                         </div>
                     </div>
